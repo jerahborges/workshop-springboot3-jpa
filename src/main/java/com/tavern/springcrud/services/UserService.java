399788433d7d36acfbc725c9e6fcfc5dd39f7f96
Jerah Borges <jerah_borges@hotmail.com>
2023-08-30 13:43:07 -0300

    Service layer, component registration

---
+++ src/main/java/com/tavern/springcrud/services/UserService.java
@@ -0,0 +1,26 @@
+package com.tavern.springcrud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tavern.springcrud.entities.User;
+import com.tavern.springcrud.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> finAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
