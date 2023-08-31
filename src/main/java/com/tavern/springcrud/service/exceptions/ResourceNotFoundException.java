60fd32af8ec04dc876e50f7eb201675db9ddb327
Jerah Borges <jerah_borges@hotmail.com>
2023-08-31 09:42:47 -0300

    Exception handling - findById

---
+++ src/main/java/com/tavern/springcrud/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tavern.springcrud.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. id " + id);
+	}
+}
